0db3d2bf0e68c8db6bc17cc830bd87e8ed41cc5d
RamRaj1440 <102046374+RamRaj1440@users.noreply.github.com>
2023-12-22 20:22:34 +0530

    Update random password generator.py
    create  random password generator using python development

--- random password generator.py
+++ random password generator.py
@@ -36,3 +36,12 @@ while(True):
 password = []
 
 for i in range(length):
+# Picking a random character from our 
+	# character list
+	randomchar = random.choice(characterList)
+	
+	# appending a random character to password
+	password.append(randomchar)
+
+# printing password as a string
+print("The random password is " + "".join(password))
